31698cafa5aed89ebe93e25b9fac25ef620842a0
fioalpha <fioalpha@gmail.com>
2021-03-19 14:58:18 -0300

    create repository interface

--- front-end/android/app/src/main/java/com/magalu/platform/grpc_test/data/Repository.kt
+++ front-end/android/app/src/main/java/com/magalu/platform/grpc_test/data/Repository.kt
@@ -1,4 +1,10 @@
 package com.magalu.platform.grpc_test.data
 
+import com.magalu.platform.grpc_test.domain.model.Task
+import io.reactivex.rxjava3.core.Single
+
 interface Repository {
+    fun getAllTasks(): Single<List<Task>>
+    fun deleteTask(task: Task): Single<Task>
+    fun saveTask(task: Task): Single<Task>
 }
